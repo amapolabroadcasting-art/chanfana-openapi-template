fe4ad6fd8905b0bea29369b08856ead23e8cd2ff
amapolabroadcasting-art <amapolabroadcasting@gmail.com>
2025-12-06 22:22:14 +0800

    df

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,23 @@ const openapi = fromHono(app, {
 		},
 	},
 });
+openapi.use("*", async (c:any, next: any) => {
+  await next();
 
+  c.res.headers.set("Access-Control-Allow-Origin", "*");
+  c.res.headers.set("Access-Control-Allow-Headers", "*");
+  c.res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  return c.res;
+});
+
+openapi.options("*", (c:any) => {
+  return c.text("", 204, {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  });
+});
 // Register Tasks Sub router
 openapi.route("/tasks", tasksRouter);
 openapi.post("/email/demo", SendDemoEmail);
