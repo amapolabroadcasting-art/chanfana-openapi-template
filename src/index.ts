f0557647e17bd5e7dc929cc3918e5029b498171f
amapolabroadcasting-art <amapolabroadcasting@gmail.com>
2025-12-06 21:29:11 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 import { tasksRouter } from "./endpoints/tasks/router";
 import { ContentfulStatusCode } from "hono/utils/http-status";
 import { DummyEndpoint } from "./endpoints/dummyEndpoint";
-
+import { SendDemoEmail } from "./endpoints/SendDemoEmail";
 // Start a Hono app
 const app = new Hono<{ Bindings: Env }>();
 
